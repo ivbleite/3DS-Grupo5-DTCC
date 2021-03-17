be0c085eee388fbda04792ab8965aa2bd8e47a53
ivbleite <ivbleite@gmail.com>
2021-03-17 15:36:42 -0300

    iniciando

--- App.js
+++ App.js
@@ -5,7 +5,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Workline
+        
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
