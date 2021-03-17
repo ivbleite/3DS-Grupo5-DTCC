f386b7c7e68f3f4c3963e6326e3593fe35cad219
ivbleite <ivbleite@gmail.com>
2021-03-17 15:39:00 -0300

    1

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Workline
+      <Text>Workline1
         
       </Text>
       <StatusBar style="auto" />
