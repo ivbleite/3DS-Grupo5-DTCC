33e13fea2998df8bded61e521a0cb327f6e0317c
ivbleite <ivbleite@gmail.com>
2021-03-22 22:27:28 -0300

    app esqueleto

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Workline12        
+      <Text>Workline10        
       </Text>
       <StatusBar style="auto" />
     </View>
